08bd12e2f6967facef38db256a0f99b71acb8b55
Quoc Doan <mythicalprogrammer@gmail.com>
2025-03-30 19:22:05 -0700

    show timestamps in correct timezone

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -92,7 +92,12 @@ defmodule SlaxWeb.ChatRoomLive do
         </div>
       </div>
       <div id="room-messages" class="flex flex-col grow overflow-auto" phx-update="stream">
-        <.message :for={{dom_id, message} <- @streams.messages} message={message} dom_id={dom_id} />
+        <.message 
+          :for={{dom_id, message} <- @streams.messages} 
+          message={message} 
+          dom_id={dom_id} 
+          timezone={@timezone}
+        />
       </div>
       <div class="h-12 bg-white px-4 pb-4">
         <.form
@@ -122,6 +127,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
+  attr :timezone, :string, required: true
 
   defp message(assigns) do
     ~H"""
@@ -132,7 +138,9 @@ defmodule SlaxWeb.ChatRoomLive do
            <.link class="text-sm font-semibold hover:underline">
              <span>{username(@message.user)}</span>
            </.link>
-		   <span class="ml-1 text-xs text-gray-500">{message_timestamp(@message)}</span>
+           <span :if={@timezone} class="ml-1 text-xs text-gray-500">
+             {message_timestamp(@message, @timezone)}
+           </span>
            <p class="text-sm">{@message.body}</p>
          </div>
        </div>
@@ -144,8 +152,9 @@ defmodule SlaxWeb.ChatRoomLive do
     user.email |> String.split("@") |> List.first() |> String.capitalize()
   end
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
 	message.inserted_at
+    |> Timex.Timezone.convert(timezone)
 	|> Timex.format!("%-l:%M %p", :strftime)
   end
 
@@ -172,7 +181,9 @@ defmodule SlaxWeb.ChatRoomLive do
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms()
 
-    {:ok, assign(socket, rooms: rooms)}
+    timezone = get_connect_params(socket)["timezone"]
+
+    {:ok, assign(socket, rooms: rooms, timezone: timezone)}
   end
 
   def handle_params(params, _uri, socket) do
